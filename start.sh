fffd2ef54c749fe519297bcf1d0b2657bef02e32
efkot-dev <fggfrew@gmail.com>
2026-09-22 18:01:10 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,59 +13,55 @@ get_mac_address() {
 
 increment_mac() {
     mac=$1
-    mac_no_colons=$(echo "$mac" | tr -d ':' | tr '[:lower:]' '[:upper:]')
+    mac_no_colons=$(echo "$mac" | tr -d ':' | tr '[:upper:]' '[:lower:]')
     mac_dec=$((16#${mac_no_colons}))
     mac_dec=$((mac_dec + 1))
-    mac_hex=$(printf "%012X" "$mac_dec")
+    mac_hex=$(printf "%012x" "$mac_dec")
     echo "$mac_hex" | sed 's/.\{2\}/&:/g;s/:$//'
 }
 
 echo "" > /etc/qemu/bridge.conf
 
-qemu="qemu-system-x86_64"
-if [[ `uname -m` == "aarch64" ]]; then
-   qemu="qemu-system-aarch64 -pflash /usr/share/qemu/edk2-aarch64-code.fd -M virt"
-fi
+# Явно указываем q35, чтобы избежать конфликтов legacy IDE (bus=0, unit=0)
+qemu="qemu-system-x86_64 -machine q35"
 
 if [ -e /dev/kvm ]; then
     qemu="$qemu -enable-kvm -cpu host"
-else
-    if [[ `uname -m` == "aarch64" ]]; then
-        qemu="$qemu -cpu cortex-a57"
-    fi
 fi
 
-img=`ls /diskimage/*.img`
-qemu="$qemu -nographic -m $MEM -smp cpus=$CPUS -drive file=$img,format=raw"
+img=`ls diskimage/*.img`
+
+# Явно указываем формат и интерфейс virtio для диска
+qemu="$qemu -nographic -m $MEM -smp cpus=$CPUS -drive file=$img,format=raw,if=virtio"
+
 truncate -s $DISK $img
 
 while read -r line; do
     iface=$(echo "$line" | sed "s/ *\([a-z0-9]*\):.*/\1/")
-    if [[ "$iface" =~ " " ]]; then
+    if [[ "$iface" =~ " " ]] || [[ "$iface" =~ ^br ]] || [[ "$iface" == "lo" ]] || [[ "$iface" == "" ]]; then
         continue
     fi
-    if [[ "$iface" =~ ^br ]]; then
-        continue
-    fi
-    if [[ "$iface" == "lo" ]]; then
-        continue
-    fi
-    if [[ "$iface" == "" ]]; then
-        continue
-    fi
+    
     echo "iface: $iface"
     bridge="br$bridge_index"
-    ip link add name "$bridge" type bridge
+    
+    # Проверяем, существует ли мост, чтобы не падать с RTNETLINK File exists
+    if ! ip link show "$bridge" >/dev/null 2>&1; then
+        ip link add name "$bridge" type bridge
+    fi
     ip link set dev "$bridge" up
-    ip link set "$iface" master "$bridge"
+    
+    if ! ip link show "$iface" master "$bridge" >/dev/null 2>&1; then
+        ip link set "$iface" master "$bridge"
+    fi
+    
     echo "allow $bridge" >> /etc/qemu/bridge.conf
 
     veth_mac=$(get_mac_address "$iface")
     tap_mac=$(increment_mac "$veth_mac")
 
     bridge_index=$((bridge_index + 1))
-    qemu="$qemu -netdev bridge,id=$bridge,br=$bridge -device virtio-net,netdev=$br
-idge,mac=$tap_mac "
+    qemu="$qemu -netdev bridge,id=$bridge,br=$bridge -device virtio-net-pci,netdev=$bridge,mac=$tap_mac"
 done < /proc/net/dev
 
 echo "qemu: $qemu"
